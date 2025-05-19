c297bd6640d8d418dfe19feca992a41e4c4a1e52
EMajdalani <EMajdalani121@gmail.com>
2025-05-19 17:54:52 -0400

    Added split block function and test cases

---
+++ src/split_blocks.py
@@ -0,0 +1,10 @@
+
+
+def markdown_to_blocks(markdown):
+    raw_blocks = markdown.split("\n\n")
+    new_blocks = []
+    for block in raw_blocks:
+        new_block = block.strip()
+        if new_block != "":
+            new_blocks.append(new_block)
+    return new_blocks
