e513c5c48d6befd0466f6fb1a3682efb792d57fd
EMajdalani <EMajdalani121@gmail.com>
2025-06-17 20:29:11 -0400

    created copy static function

---
+++ main.py
@@ -0,0 +1,33 @@
+import shutil
+import os
+
+def main():
+    static_path = "static"
+    public_path = "public"
+    
+    copy_static(static_path, public_path)
+
+def copy_recursive(source_dir, dest_dir):
+    source_dir_list = os.listdir(source_dir)
+    for item in source_dir_list:
+        item_path = os.path.join(source_dir, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, dest_dir)
+        elif os.path.isdir(item_path):
+            new_source_dir = item_path
+            new_dest_dir = os.path.join(dest_dir, item)
+            os.mkdir(new_dest_dir)
+            copy_recursive(new_source_dir, new_dest_dir)
+    
+    return
+
+def copy_static(source_dir, dest_dir):
+    if os.path.isdir(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+    copy_recursive(source_dir, dest_dir)
+    
+    return
+        
+
+main()
